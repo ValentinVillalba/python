d9e0b64ce3f79135e0c065072771108a364fe6b3
Valentin Villalba <75283418+ValentinVillalba@users.noreply.github.com>
2022-01-26 04:19:24 +0000

    Update is_valid_walk.py

--- is_valid_walk.py
+++ is_valid_walk.py
@@ -1,11 +1,9 @@
 def is_valid_walk(walk):
     dic = {"n":0,"s":0,"e":0,"w":0}
-    steps = 0
     for i in walk:
         if(i in dic.keys()):
             dic[i] += 1
-            steps += 1
-    return (steps == 10 and dic["n"] == dic["s"] and dic["e"] == dic["w"])
+    return (len(walk) == 10 and dic["n"] == dic["s"] and dic["e"] == dic["w"])
   
 walk = ["n","s","n","e","o","s"]
 is_valid_walk(walk)

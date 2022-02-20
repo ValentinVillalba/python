ee8a01532159a883a4a5beeda54181ecae486d82
Valentin Villalba <75283418+ValentinVillalba@users.noreply.github.com>
2022-02-20 17:39:21 +0000

    Update numeric_true_false.py

--- numeric_true_false.py
+++ numeric_true_false.py
@@ -3,7 +3,7 @@ def smallerOne(a,b):
 	#funciona haciendo un calculo en el que uno de los dos lados dara como resultado 0
 	#y el otro lado dara como resultado el numero ingresado y se retornará.
 	#Hay que recordar que True y False, tambien son 1 y 0, respectivamente.
-	#Es un ejemplo de funcion sin ramas, evitando usar "if" y acelerando el programa.
+	#Es un ejemplo de funcion sin ramas, evitando usar "if".
 	return a * (a < b) + b * (b <= a)
 
 print(smallerOne(56,23))

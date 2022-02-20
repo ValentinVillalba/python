438c6925524d33b5f25396fba90b69b9a655bd91
Valentin Villalba <75283418+ValentinVillalba@users.noreply.github.com>
2022-02-20 17:11:57 +0000

    Update two_characters_final.py

--- two_characters_final.py
+++ two_characters_final.py
@@ -1,6 +1,6 @@
 def solution(s):
     result = []
-    #LOS PARAMETROS SE PUEDEN EDITAR Y QUEDAN GUARDADOS!!!
+    #LOS PARAMETROS SE PUEDEN EDITAR Y QUEDAN GUARDADOS
     if len(s) % 2:
         s += '_'
     for i in range(0, len(s), 2): #EN RANGE TAMBIEN SE PUEDE HACER DESDE, HASTA Y PASO
